89d3c1c2bd167057221f039335785b5e41d0e398
Thomas Fricard <thomasfricard@gmail.com>
2022-02-18 16:01:23 +0100

    models creation

--- epic_events/crm/models.py
+++ epic_events/crm/models.py
@@ -1,18 +1,56 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
-TEAM_CHOICES = ['sales', 'support',  'management']
 PHONE_VALIDATOR = RegexValidator(r"^\+?1?\d{8,15}$")
 
 
-class User(models.Model):
+class Employee(models.AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    team = models.Charfield(choices=TEAM_CHOICES, max_length=50)
 
 
-class Client(models.Model):
+class Assignee(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    employee = models.ForeignKey(
+        'Employee',
+        on_delete=models.CASCADE,
+        related_name='employees'
+    )
+
+
+class EventAssignation(Assignee):
+    event = models.ForeignKey(
+        'Event',
+        on_delete=models.CASCADE,
+        related_name='event'
+    )
+
+
+class ContractAssignation(Assignee):
+    contract = models.ForeignKey(
+        'Contract',
+        on_delete=models.CASCADE,
+        related_name='contract'
+    )
+
+
+class ClientAssignation(Assignee):
+    client = models.ForeignKey(
+        'Client',
+        on_delete=models.CASCADE,
+        related_name='client'
+    )
+    is_converted = models.BooleanField()
+    medium = models.CharField(max_length=100)
+
+
+class Time(models.Model):
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField()
+
+
+class Client(models.Model, Time):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
@@ -28,27 +66,18 @@ class Client(models.Model):
         blank=True
     )
     company_name = models.CharField(max_length=100)
-    date_created = models.DateTimeField(auto_now=True)
-    date_updated = models.DateTimeField()
-    sales_contact = models.ForeignKey('User', on_delete=models.SET_NULL)
+    job = models.CharField(max_length=100)
 
 
-class Contract(models.Model):
-    date_created = models.DateTimeField(auto_now=True)
-    date_updated = models.DateTimeField()
-    status = models.BooleanField()
+class Contract(models.Model, Time):
+    is_signed = models.BooleanField()
     amount = models.FloatField()
     payment_due = models.DateTimeField()
-    sales_contact = models.ForeignKey('User', on_delete=models.SET_NULL)
     client = models.ForeignKey('Client', on_delete=models.CASCADE)
 
 
-class Event(models.Model):
-    date_created = models.DateTimeField(auto_now=True)
-    date_updated = models.DateTimeField()
+class Event(models.Model, Time):
     attendees = models.IntegerField()
     event_date = models.DateTimeField()
     notes = models.TextField()
-    event_status = models.ForeignKey('Contract', on_delete=models.CASCADE)
-    client = models.ForeignKey('Client', on_delete=models.CASCADE)
-    support_contact = models.ForeignKey('Client', on_delete=models.SET_NULL)
+    event = models.ForeignKey('Contract', on_delete=models.CASCADE)
